872cbf9eb4f91bfd8b32072be4cdab80be847689
safroniv <safroniv@mail.ru>
2022-04-23 14:52:47 +0300

    GB - ООП Урок 6 Задание 1 - готово.

--- JobLessonOOP06v03Part01/Program.cs
+++ JobLessonOOP06v03Part01/Program.cs
@@ -11,11 +11,14 @@ namespace JobLessonOOP06v03Part01
         static void Main(string[] args)
         {
             BankAccount mybankAccount1 = new BankAccount(50000.99m);
-            mybankAccount1.Print();
             BankAccount mybankAccount2 = new BankAccount(TypeBankAccount.Debet);
-            mybankAccount2.Print();
-            mybankAccount1.Transfer(mybankAccount2, 500);
-
+            //Тестирование функционирования переопределенных методов и операторов
+            Console.WriteLine(mybankAccount1.ToString());
+            Console.WriteLine(mybankAccount2.ToString());
+            Console.WriteLine(mybankAccount1.Equals(mybankAccount2));
+            Console.WriteLine(mybankAccount2.Equals(mybankAccount1));
+            Console.WriteLine(mybankAccount1.GetHashCode());
+            Console.WriteLine(mybankAccount2.GetHashCode());
             Console.ReadLine();
         }
     }
